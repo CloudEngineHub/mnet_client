87ca600c18aa8977f58a9bf716a90621228c1d1f
cheneating716 <chenyiting716@gmail.com>
2025-10-24 12:43:06 -0500

    update local client

--- mnet_client/clients/local_test_client.py
+++ mnet_client/clients/local_test_client.py
@@ -164,8 +164,32 @@ class LocalTestClient(BaseClient):
                 )
                 exit()
 
+            entry_level_tasks = {k: v for k, v in self.task_metadata.items() if v.get("level") == "entry"}
+            easy_level_tasks = {k: v for k, v in self.task_metadata.items() if v.get("level") == "easy"}
+            medium_level_tasks = {k: v for k, v in self.task_metadata.items() if v.get("level") == "medium"}
+            hard_level_tasks = {k: v for k, v in self.task_metadata.items() if v.get("level") == "hard"}
+            
+            def load_random_task(task_id: str, task_pool: dict) -> dict:
+                if task_id.startswith("L"):
+                    task_rnd = random.choice(list({k: v for k, v in task_pool.items() if v.get("mode") == "L"}.keys()))
+                elif task_id.startswith("VL"):
+                    task_rnd = random.choice(list({k: v for k, v in task_pool.items() if v.get("mode") == "VL"}.keys()))
+                else:
+                    task_rnd = random.choice(list({k: v for k, v in task_pool.items() if v.get("mode") == "V"}.keys()))
+                task = task_pool.pop(task_rnd)
+                return task
+            
             for task_id in self.scoring_details_list:
-                language_instruction = self.task_metadata[task_id]["description"]
+                if self.scoring_details[task_id] == 1:
+                    task = load_random_task(task_id, entry_level_tasks)
+                elif self.scoring_details[task_id] == 2:
+                    task = load_random_task(task_id, easy_level_tasks)
+                elif self.scoring_details[task_id] == 5:
+                    task = load_random_task(task_id, medium_level_tasks)
+                elif self.scoring_details[task_id] == 10:
+                    task = load_random_task(task_id, hard_level_tasks)
+
+                language_instruction = task["description"]
                 self.language_instructions.append(
                     language_instruction if language_instruction is not None else ""
                 )
@@ -174,7 +198,7 @@ class LocalTestClient(BaseClient):
                     "assets",
                     f"{self.task_name}",
                     "images",
-                    f"{self.task_metadata[task_id]['image']}",
+                    f"{task['image']}",
                 )
                 self.vision_instructions.append(
                     cv2.imread(image_path, cv2.IMREAD_COLOR)
